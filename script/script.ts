ddaa0727d7dc513f51aef512889b26772286b0ab
Lianna Khachatryan <lianna.khachatryan@softconstruct.com>
2021-10-20 20:35:15 +0400

    some changes

--- script/script.ts
+++ script/script.ts
@@ -1,1 +1,15 @@
-console.log('hello');
+// const rsvpButton=document.querySelector('.rsvp__button');
+// const confirm = document.querySelector('.going');
+// const rebut = document.querySelector('.cant__come');
+//  const modal = document.querySelector('.modal__container');
+
+// confirm.addEventListener('click', getData);
+// rebut.addEventListener('click', getData);
+// rsvpButton.onclick=clickRsvpButton;
+
+// function clickRsvpButton() {
+//     modal.classList.add("show");
+// }
+// function getData() {
+//     modal.classList.remove("show");
+// }
